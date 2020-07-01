c39d6e4a5911752f5312b3d436a257ce10991234
kiransm74 <67453662+kiransm74@users.noreply.github.com>
2020-07-01 16:01:17 +0530

    Initial set on 01-Jul
    This is the initial set of files committed on 01-Jul-2020.

---
+++ Kiran/FirstClass.java
@@ -0,0 +1,18 @@
+package com.philips;
+
+public class FirstClass {
+  public static void main(final String[] args) {
+    final int age = 34;
+
+    if ((age > 0) && (age < 13)) {
+      System.out.println("You are a kid");
+    } else if ((age >= 13) && (age < 20)) {
+      System.out.println("You are a teenager");
+    } else if (age >= 20) {
+      System.out.println("You are an adult");
+    } else {
+      System.out.println("You don't exist!");
+    }
+  }
+
+}
---
+++ Kiran/OperatorTest.java
@@ -0,0 +1,44 @@
+package com.philips;
+
+public class OperatorTest {
+
+  public static void main(final String[] args) {
+    // Conditional operators
+    // > - greater than
+    // < - lesser than
+    // == - equals to
+    // != - not equal to
+    // >= - greater than or equal to
+    // <= - lesser than or equal to
+    // ! - (only for boolean) - negation
+
+    /*
+     * Binary operators && - check if both left-side & right-side are true, only then condition becomes true, else, it's
+     * always false || - check if both left-side & right-side are false, only then condition becomes false, else it's
+     * always true
+     */
+
+    final int age = 18;
+    final int salary = 500000;
+
+    final boolean validAge = (age >= 0);
+
+    if (!validAge) {
+      System.out.println("Not a valid age!");
+      System.exit(1);
+    } else {
+      // Do something
+      if ((age <= 18) && (salary <= 10000)) {
+        System.out.println("It's ok, you still have time. Work hard!");
+      } else {
+        System.out.println("Your age is greater than 18, and/or your salary is greater than 10000");
+        if (salary != 500000) {
+          System.out.println("You are not yet at the CxO position!");
+        } else {
+          System.out.println("You made it! You are at the CxO position!");
+        }
+      }
+    }
+  }
+
+}
