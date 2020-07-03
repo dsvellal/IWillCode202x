33b2dbbd55a11a14ecffc4ef07d0ba23e2208cf9
Rahulkshirsagar <67453683+Rahulkshirsagar@users.noreply.github.com>
2020-07-03 11:16:56 +0530

    Create Company.java
    public class Company
    Organizations structure

---
+++ Rahul/Company.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+
+public class Company{
+    
+    public static void main(String[]args){
+
+   char character = 'p'; 
+        int organisationlevel= 3;
+        String myJobName = "Quality engineer";
+        String myBusinesscategoryName = "Enterprices Integrated Solution";
+        String myBusinessunitName = "Connected care informatics";
+        String myClusterName = "Connected care";
+        
+        System.out.println(character);
+        System.out.println(organisationlevel);
+        System.out.println(myJobName);
+        System.out.println(myClusterName);
+        System.out.println(myBusinessunitName);
+        System.out.println(myBusinesscategoryName);
+    }
+
+}
+
