168fd78f50691aca6f525f40ae65c198e464be11
Rahulkshirsagar <67453683+Rahulkshirsagar@users.noreply.github.com>
2020-07-03 10:43:39 +0530

    Create Philips.java
    public class Transportation
    Conditional statement programming

---
+++ Rahul/Philips.java
@@ -0,0 +1,13 @@
+public class Transportation{
+
+     public static void main(String []args){
+         
+        final boolean officeTransportationIsAvailable = false;
+        if (officeTransportationIsAvailable) {
+         
+        System.out.println(" I will take office bus");
+        } else {
+        System.out.println(" I will take ola cab");    
+        }
+     }}
+
