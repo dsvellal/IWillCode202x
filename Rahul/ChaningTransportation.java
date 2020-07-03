312ae7c0f6a0dd71e348d2fa332f3875fcb26c5f
Rahulkshirsagar <67453683+Rahulkshirsagar@users.noreply.github.com>
2020-07-03 10:57:03 +0530

    Create ChaningTransportation.java
    public class Transportation
    Conditional programming chaining

---
+++ Rahul/ChaningTransportation.java
@@ -0,0 +1,18 @@
+public class Transportation{
+
+     public static void main(String []args){
+         
+        final boolean officeTransportationIsAvailable = false;
+        final boolean olaCabIsAvailable = false;
+        if (officeTransportationIsAvailable){
+         
+        System.out.println(" I will take office bus");
+        }else if(olaCabIsAvailable){      
+    
+        System.out.println(" I will take ola cab");    
+        } else{
+        System.out.println("I will go with my friend");    
+        }
+     }
+    
+}
