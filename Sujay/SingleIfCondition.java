47668e6e1e3e0d9d7dc50b4b43d00f31f0d5d77b
sujay-blueheart <67774196+sujay-blueheart@users.noreply.github.com>
2020-07-03 10:47:33 +0530

    Programming using Single If Condition

---
+++ Sujay/SingleIfCondition.java
@@ -0,0 +1,11 @@
+package com.blueheart;
+
+public class SingleIfCondition {
+	public static void main(String[] args) {
+		int acccountBalance = 1900;
+
+		if(acccountBalance < 2000) {
+			System.out.println("You account balance is below the minimum balance expected");
+		}
+	}
+}
