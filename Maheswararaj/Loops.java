fdf5afc487916523c02a6d375de47e248c121fdb
Mahesh <jmaheswararaj@gmail.com>
2020-07-03 10:28:35 +0530

    Code using loops
    As part of IWillCode intiative learning today how to use loops in Java

---
+++ Maheswararaj/Loops.java
@@ -0,0 +1,25 @@
+Public class Loops{
+  public static void main(final String[] args) {
+    // Loops.
+    // for, while, do-while
+    // for - traditional for loop, enhanced for loops
+ 
+    // arrays
+    final String[] namesOfParticipantsOfIWillCode =
+        {"Datta", "Kiran", "Sujay", "Sobi", "Jabeen", "KLM", "Pavithra", "Sapna"};
+ 
+ 
+    // Traditional for loop
+    // Loop has three elements: 1. Initialisation, 2. Condition for termination, 3.
+    // Increment/decrement
+    for (int i = 0; i < namesOfParticipantsOfIWillCode.length; i++) { // i-- ==> i = i-1
+      System.out.println(namesOfParticipantsOfIWillCode[i].toUpperCase());
+    }
+ 
+    // Enhanced for loop
+    // No initlisation, no increment, no termination, no index-access of elements!
+    for (final String nameOfParticipantofIWillCode : namesOfParticipantsOfIWillCode) {
+      System.out.println(nameOfParticipantofIWillCode.toLowerCase());
+    }
+}
+}
