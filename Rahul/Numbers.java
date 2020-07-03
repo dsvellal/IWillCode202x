c749cb7be6c69943e50e5a248809eacf460be3ce
Rahulkshirsagar <67453683+Rahulkshirsagar@users.noreply.github.com>
2020-07-03 11:07:30 +0530

    Create Numbers.java
    public class Numbers

---
+++ Rahul/Numbers.java
@@ -0,0 +1,24 @@
+public class numbers {
+  public static void main(final String[] args) {
+
+    
+    int num1 = 100;
+    int num2 = 200;
+    int num3 = 300;
+    int num4 = 400;
+    
+  
+        if (num1 >= num2 && num1 >= num3){
+           if (num1>= num4)
+          System.out.println(num1+"is the greatest number!");
+        } else {
+          System.out.println(num3+"is the greatest number!");
+        }
+        if (num3>=num2){
+          System.out.println(num3+"is the greatest number!");    
+        }
+        else {
+          System.out.println(num2+"is the greatest number!");    
+        }
+      }
+    }
