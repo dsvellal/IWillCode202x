5fdaae172dcae98a33fb6d115768ed31ebb66568
kiransm74 <67453662+kiransm74@users.noreply.github.com>
2020-07-03 18:30:46 +0530

    This is second revision.

--- Kiran/On20200703Simply.java
+++ Kiran/On20200703Simply.java
@@ -1,8 +1,11 @@
 package com.IWillCode;
-// This is the first revision.
+//This is the second revision with variable.
 
 public class On20200703Simply {
+  static int somevar = 10;
+
   public static void main(final String[] args) {
     System.out.println("This is simply made to see github work.");
+    System.out.println("Variable defined has value: " + somevar);
   }
 }
