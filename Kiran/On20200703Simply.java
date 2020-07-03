7fa61f188cd142ecc18a797f6ca33dc2fe1d7b36
kiransm74 <67453662+kiransm74@users.noreply.github.com>
2020-07-03 18:25:54 +0530

    First revision was committed.

---
+++ Kiran/On20200703Simply.java
@@ -0,0 +1,8 @@
+package com.IWillCode;
+// This is the first revision.
+
+public class On20200703Simply {
+  public static void main(final String[] args) {
+    System.out.println("This is simply made to see github work.");
+  }
+}
