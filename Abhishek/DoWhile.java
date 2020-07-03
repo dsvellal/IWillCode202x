ea231e6b3c1907b2157f98671a6d7930fabd292c
01kumarabhishek <67453747+01kumarabhishek@users.noreply.github.com>
2020-07-03 10:34:22 +0530

    DoWhileexample

---
+++ Abhishek/DoWhile.java
@@ -0,0 +1,30 @@
+package com.philips.sample;
+
+public class DoWhile {
+	public static void main(final String[] args) {
+	    // 1. Initialisation
+	    // 2. Termination condition
+	    // 3. Increment/decrement
+
+	 
+
+	    // for loop - all three happens in 1-line
+	    // while loop - all three happens in 3 different lines
+	    // do while loop - all three happens in 3 lines
+
+	    final int length = 1;
+	    int i = 0; // Initialisation
+	    do {
+	      System.out.println("do-while i=" + i);
+	      i = i + 1; // Increment
+	    } while (i < length); // Condition
+
+	 
+
+	    int j = 0; // Initialisation
+	    while (j < length) { // Condition
+	      System.out.println("while j=" + j);
+	      j = j + 1; // Increment
+	    }
+	  }
+}
