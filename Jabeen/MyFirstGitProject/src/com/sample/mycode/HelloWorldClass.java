ea930907323d59159dc1861f33ad6b6fdb1d9fa9
Jabeen <musarrath.jabeen.gn@philips.com>
2020-07-10 11:19:54 +0530

    My First Git Commit

---
+++ Jabeen/MyFirstGitProject/src/com/sample/mycode/HelloWorldClass.java
@@ -0,0 +1,9 @@
+package com.sample.mycode;
+
+public class HelloWorldClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World ");
+	}
+}
---
+++ Jabeen/MyFirstGitProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author 310225932
+ *
+ */
+module MyFirstGitProject {
+}
