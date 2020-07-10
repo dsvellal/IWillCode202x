edf0661771c5e3f9df4c04ad846f3eb6c12368ae
Abhishek <kumar.abhishek@philips.com>
2020-07-10 11:19:55 +0530

    my first git commit

---
+++ Abhishek/My_git_Project/src/com/sample/my_code/Hello_world.java
@@ -0,0 +1,10 @@
+package com.sample.my_code;
+
+public class Hello_world {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ Abhishek/My_git_Project/src/module-info.java
@@ -0,0 +1,2 @@
+module My_git_Project {
+}
