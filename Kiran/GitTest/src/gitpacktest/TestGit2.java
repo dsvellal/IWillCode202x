361d72fef5978bad3b97d3001157bba3631faf89
Mirle <kiran.mirle@philips.com>
2020-07-15 17:56:51 +0530

    Print line added to TestGit2.java

--- Kiran/GitTest/src/gitpacktest/TestGit2.java
+++ Kiran/GitTest/src/gitpacktest/TestGit2.java
@@ -5,6 +5,7 @@ public class TestGit2 {
 	public static void main(String[] args) {
 		int num1 = 10 ;
 		System.out.println("The value of num1 is: " + num1) ;
+		System.out.println("This sentence was added on 15-Jul-2020.");
 
 	}
 
