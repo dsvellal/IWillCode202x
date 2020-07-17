82448e44f1514e31a858ac4e0e30b07c7f4778b4
Gangiredla <srinivas.gangiredla@philips.com>
2020-07-17 11:46:08 +0530

    test

---
+++ Srinivas/First_Project/src/com/sample/test/ExampleMinNumber.java
@@ -0,0 +1,30 @@
+package com.sample.test;
+
+public class ExampleMinNumber
+{
+	
+	
+	public static void main(String [] args)
+	{
+		int a = 11;
+		int b = 6;
+		int c = minFunction(a, b);
+		System.out.println("Minimum value " +c);
+		
+	}
+
+	
+	public static int minFunction(int n1, int n2)
+	{
+		int min;
+		if(n1>n2)
+		{
+			min = n2;
+		}
+		else
+		{
+			min = n1;
+		}
+		return min;
+	}
+}
