634288832198d1fa87405a062e5288529bacf138
Gangiredla <srinivas.gangiredla@philips.com>
2020-07-24 11:19:46 +0530

    Corrected the code as per review

--- Srinivas/First_Project/src/com/sample/test/Scannero.java
+++ Srinivas/First_Project/src/com/sample/test/Scannero.java
@@ -1,5 +1,21 @@
 package com.sample.test;
 
-public class Scannero {
 
+import java.util.*;
+public class Scannero 
+{
+	public static void main(String [] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("enter number");
+		try {
+		int number = scanner.nextInt();
+		System.out.println("Enterned number is " +number);}
+		catch(Exception e)
+		{
+			System.out.println("Sorry you had entered, something not integer");
+		}
+		
+		
+	}
 }
