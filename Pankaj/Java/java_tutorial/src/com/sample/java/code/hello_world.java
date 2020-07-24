13d19f9a951615112e44c0d4d1f683d25d84071d
prasadpn <pankaj.nprasad@gmail.com>
2020-07-24 11:00:28 +0530

    try catch test

--- Pankaj/Java/java_tutorial/src/com/sample/java/code/hello_world.java
+++ Pankaj/Java/java_tutorial/src/com/sample/java/code/hello_world.java
@@ -1,7 +1,32 @@
 package com.sample.java.code;
 
+import java.util.Scanner;
+
 public class hello_world {
-	public static void main(String[] args) {
-		System. out. println("Hello, World!");
-		}
+
+  public static void main(String[] args) {
+
+    final Scanner scanner = new Scanner(System.in);
+
+
+ 
+
+    System.out.println("Please enter a number: ");
+
+    try {
+
+      final int number = scanner.nextInt();
+
+      System.out.println("Entered number is: " + number);
+
+    } catch (final Exception e) {
+
+      System.out.println("Sorry, you entered something that's not an integer!");
+
+      System.out.println("Please run the program again, and enter an integer");
+
+    }
+
+  }
+
 }
