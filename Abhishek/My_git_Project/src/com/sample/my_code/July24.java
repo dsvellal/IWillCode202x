6407c6a355158d78f1e6d44a8791574fa5021d12
Abhishek <kumar.abhishek@philips.com>
2020-07-24 10:53:47 +0530

    TryCatch example

---
+++ Abhishek/My_git_Project/src/com/sample/my_code/July24.java
@@ -0,0 +1,23 @@
+package com.sample.my_code;
+
+import java.util.Scanner;
+
+public class July24 {
+ 
+	  public static void main(String[] args) {
+
+	    final Scanner scanner = new Scanner(System.in);	 
+
+	    System.out.println("Please enter a number: ");
+try{
+	    final int number = scanner.nextInt();	 
+
+	    System.out.println("Entered number is: " + number);
+
+	  } catch (Exception e) {
+		  System.out.println("Sorry, you Ebter something that is not a integer!");
+		  System.out.println("Please rerun the program again, and enter an integer");
+	  }
+
+	}
+}
