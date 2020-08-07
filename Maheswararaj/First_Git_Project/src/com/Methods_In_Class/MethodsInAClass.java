5860c30c318cee8387cf6d714f641b836e58ca38
Jeyarajan <maheswararaj.j@philips.com>
2020-08-07 11:36:07 +0530

    setname & fetchname another example added

--- Maheswararaj/First_Git_Project/src/com/Methods_In_Class/MethodsInAClass.java
+++ Maheswararaj/First_Git_Project/src/com/Methods_In_Class/MethodsInAClass.java
@@ -2,6 +2,7 @@ package com.Methods_In_Class;
 
 public class MethodsInAClass {
 	private String name;
+	private String name_1;
 	
 	//Syntax for creating a method
 	//access-modifier, <optional - static, final, abstract>, return-data-type, <Optional-method-arguments>
@@ -17,6 +18,11 @@ public class MethodsInAClass {
 		MethodsInAClass instance = new MethodsInAClass();
 		instance.setname("Mahesh");
 		System.out.println(instance.fetchname());
+		
+		MethodsInAClass instance_1 = new MethodsInAClass();
+		instance_1.setname("Maheswararaj J");
+		System.out.println(instance_1.fetchname());
 	}
+	
 }
 	
