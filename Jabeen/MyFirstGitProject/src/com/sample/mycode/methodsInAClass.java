01c8a16e1f51234c0c11e13731907e62a513928b
Jabeen <musarrath.jabeen.gn@philips.com>
2020-08-07 11:30:37 +0530

    Added a new parameter

--- Jabeen/MyFirstGitProject/src/com/sample/mycode/methodsInAClass.java
+++ Jabeen/MyFirstGitProject/src/com/sample/mycode/methodsInAClass.java
@@ -3,6 +3,7 @@ package com.sample.mycode;
 public class methodsInAClass {
 
 	private String name;
+	private long accountNo;
 	
 	public String fetchName() {
 		return this.name;
@@ -12,11 +13,22 @@ public class methodsInAClass {
 		name = ipname;
 	}
 	
+	public long fetchAcctNo() {
+		return this.accountNo;
+	}
+	
+	public void setAcctNo(long actNo) {
+		accountNo = actNo;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Print Details \n");
 		methodsInAClass obj = new methodsInAClass();
 		obj.setName("Jabeen");
-		System.out.println("Print Name \n"+obj.fetchName());
+		System.out.println("Print Name : "+obj.fetchName());
+		
+		obj.setAcctNo(987654321);
+		System.out.println("Account No : "+obj.fetchAcctNo());
 	}
 
 }
