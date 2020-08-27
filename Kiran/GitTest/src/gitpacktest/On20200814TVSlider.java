9c75260a3e390f7f437943cab17d26299fc7bd25
Mirle <kiran.mirle@philips.com>
2020-08-27 17:58:17 +0530

    Modified TV Slider program was committed.

--- Kiran/GitTest/src/gitpacktest/On20200814TVSlider.java
+++ Kiran/GitTest/src/gitpacktest/On20200814TVSlider.java
@@ -7,7 +7,9 @@ public class On20200814TVSlider {
 	}
 	
 	public int getIncorDecVal (int vRecdValue) {
-		int vSlider = 50 ; //Why isn't the 50 in the constructor taken in this method if this line is not there?
+		int vSlider = 50 ; //Why isn't the 40 in the constructor taken in this method if this line is not there?
+		// If I try to print vSlider without initialisation here, I get an error that vSlider is not initialised.
+		System.out.println("The default value of the attribute is : " + vSlider) ;
 		vSlider = vSlider + vRecdValue ;
 		System.out.println("The new value of the attribute is : " + vSlider) ;
 		return vSlider ;
