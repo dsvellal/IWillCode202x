70f798ce243704b0c3a37b1d5a9fd657347606bd
Jeyarajan <maheswararaj.j@philips.com>
2020-08-28 19:24:26 +0530

    class with lots of methods - Bank account examples

---
+++ Maheswararaj/First_Git_Project/src/com/iwillcode/bank/AccountDetails.java
@@ -0,0 +1,41 @@
+package com.iwillcode.bank;
+
+public class AccountDetails {
+	// Store and retrieve values
+	// Credit
+	// Debit
+	private int accountNumber;
+	private String accountHolderName;
+	private double accountBalance;
+	
+	public AccountDetails(String nameOfAccountHolder) {
+		accountHolderName = nameOfAccountHolder;
+		accountBalance = 0;
+		accountNumber = accountHolderName.hashCode();
+	}
+
+	public void credit(double valueToBeAdded) {
+		if (valueToBeAdded > 0) {
+			accountBalance = accountBalance + valueToBeAdded;
+		} else {
+			System.out.println("Invalid value being credited. Operation cancelled!");
+		}
+		
+	}
+	
+	public void debit(double valueToBeDebited) {
+		if (accountBalance >= valueToBeDebited) {
+			accountBalance = accountBalance - valueToBeDebited;
+		} else {
+			System.out.println("Invalid funds");
+		}
+	}
+	
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	
+	public void printAccountBalance() {
+		System.out.println(accountBalance);
+	}
+}
