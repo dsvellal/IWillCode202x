1f158ecc8c1c50f72647acf7ee80b183de430d50
Mirle <kiran.mirle@philips.com>
2020-09-04 10:53:20 +0530

    Two bank related files are modified.

--- Kiran/GitTest/src/com/Iwillcode/bank/AccountDetails.java
+++ Kiran/GitTest/src/com/Iwillcode/bank/AccountDetails.java
@@ -6,19 +6,20 @@ public class AccountDetails {
 	//User entered properties follow
 	private long vAadhaarCard ;
 	private String vName ;
-	private Date vDoB ;
+	private String vDoB ;
 	private String vAddress ;
 	private long vMobileNumber ;
 	private String vReferer ;
+	private long vAcNumasofNow ; //This variable is meant to store the last bank account number
 	
 	// Properties set by bank follow
-	private String vBankAcNumber ;
+	private long vBankAcNumber ;
 	private String vAcctType ;
 	private String vIFSCCode ;
 	private String vBranchCode ;
 	private String vBankCode ;
 	
-	public AccountDetails (long vAadhaarCard, String vName, Date vDoB, String vAddress, long vMobileNumber, String vReferer) {
+	public AccountDetails (long vAadhaarCard, String vName, String vDoB, String vAddress, long vMobileNumber, String vReferer) {
 		//Assigning values to user entered properties
 		this.vAadhaarCard = vAadhaarCard ;
 		this.vName = vName ;
@@ -28,12 +29,14 @@ public class AccountDetails {
 		this.vReferer = vReferer ;
 		
 		//Assigning values to system generated properties
-		// this.vBankAcNumber = mgetAccountNumber();
+		this.vBankAcNumber = vAcNumasofNow + 1 ;
 		this.vAcctType = "Savings" ;
 		this.vIFSCCode = "BANK12345" ;
 		this.vBankCode = "Somebank123" ;
 		this.vBranchCode = "Manyata560045" ;
 		
 	} //This is for constructor
+	
+	
 
 } //This is for class AccountDetails
---
+++ Kiran/GitTest/src/com/Iwillcode/bank/KirAcDetTest.java
@@ -0,0 +1,14 @@
+package com.Iwillcode.bank;
+
+public class KirAcDetTest {
+
+	
+
+		public static void main(String[] args) {
+			AccountDetails dattasAccount = new AccountDetails(100000000000L, "Datta", "12/06/1985", "Girinagar", 1234567890L, "Kiran");
+
+		}
+
+	
+
+}
