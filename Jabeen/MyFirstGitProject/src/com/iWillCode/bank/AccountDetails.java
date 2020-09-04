a066006f70e551b17da8fee582394a68f042bb63
Jabeen <musarrath.jabeen.gn@philips.com>
2020-09-04 10:23:09 +0530

    Bank Account Program

---
+++ Jabeen/MyFirstGitProject/src/com/iWillCode/bank/AccountDetails.java
@@ -0,0 +1,58 @@
+package com.iWillCode.bank;
+
+import java.util.Date;
+
+public class AccountDetails {
+	//User entered variables
+	static long baseAcctNo = 100000001;
+	private long adhaarCard;
+	private String name;
+	private String address;
+	private String dob;
+	private long mobileNo;
+	private String referenceName;
+	
+	//Autoset variables
+	private long bankAcctNo;
+	private String type; //saving, current, loan, trading
+	private String ifscCode;
+	private String branchCode;
+	private String bankCode;
+	
+	public AccountDetails(long adhaarCard, String name, String address, String dob, long mobileNo, String referenceName) {
+		this.adhaarCard = adhaarCard;
+		this.name = name;
+		this.address = address;
+		this.dob = dob;
+		this.mobileNo = mobileNo;
+		this.referenceName = referenceName;
+		
+		this.bankAcctNo = getAcctNo();
+		this.type = "Savings";
+		this.ifscCode = "IFSC098765";
+		this.branchCode = "CITI-MG01";
+		this.bankCode = "CITI-BLR01";
+	}
+	
+	private long getAcctNo() {
+		baseAcctNo = baseAcctNo +1;
+		return baseAcctNo;
+	}
+	public void printAcctDetails()
+	{
+		System.out.println("\n Bank Account Details ");
+		System.out.println("=====================");
+		System.out.println("Adhaar Card no : "+adhaarCard);
+		System.out.println("Name : "+name);
+		System.out.println("Address : "+address);
+		System.out.println("Date of Birth : "+dob);
+		System.out.println("Mobile No : "+mobileNo);
+		System.out.println("Bank Account No :"+bankAcctNo);
+		System.out.println("Account Type :"+type);
+		System.out.println("IFSC Code :"+ifscCode);
+		System.out.println("Branch Code :"+branchCode);
+		System.out.println("Bank Code :"+bankCode);
+			
+	}
+		
+}
