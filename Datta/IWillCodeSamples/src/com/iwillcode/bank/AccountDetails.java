ed0ca7ce498e3c2f355fb09b31bf5f246cca8d02
Dattatreya Vellal <dsvellal@philips.com>
2020-09-25 11:16:12 +0530

    Eliminated PMD violations

--- Datta/IWillCodeSamples/src/com/iwillcode/bank/AccountDetails.java
+++ Datta/IWillCodeSamples/src/com/iwillcode/bank/AccountDetails.java
@@ -3,36 +3,14 @@ package com.iwillcode.bank;
 public class AccountDetails {
 	static long baseAccountNumber = 1000000000l;
 	// Some of these are user entered properties
-	private long aadhaarCard;
-	private String nameOfAccountHolder;
-	private String dob;
-	private String address;
-	private long mobileNumber;
-	private String refererName;
 	private double accountBalance;
 	
 	// Some of these are autoset by the bank
 	private long bankAccountNumber;
-	private String type; // Savings or current or trading or overdraft or loan
-	private String ifscCode;
-	private String branchCode;
-	private String bankCode;
+
 	
-	public AccountDetails(long aadhaarCard, String nameOfAccountHolder, String dob, String address, long mobileNumber, String refererName) {
-		// Assigning values to user entered properties
-		this.aadhaarCard = aadhaarCard;
-		this.nameOfAccountHolder = nameOfAccountHolder;
-		this.dob = dob;
-		this.address = address;
-		this.mobileNumber = mobileNumber;
-		this.refererName = refererName;
-		
-		// Assigning system generated property values
+	public AccountDetails() {
 		this.bankAccountNumber = getAccountNumber();
-		this.type = "Savings";
-		this.ifscCode = "HDFC00001750";
-		this.branchCode = "1750";
-		this.bankCode = "HDFC0000";
 		this.accountBalance = 0d;
 	}
 
